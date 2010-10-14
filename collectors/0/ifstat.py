f3e3dd7b137068b8c22f9fe5af9abed200ee9a3d
Dave Barr <barr@stumbleupon.com>
2010-10-13 23:21:28 -0700

    ifstat.py: network interface statistics
    Change-Id: I0981acb7760cc4eca0d68fba0e72aa3e7385618b
    Reviewed-on: https://review.stumble.net/598
    Reviewed-by: Dave Barr <barr@stumbleupon.com>
    Tested-by: Dave Barr <barr@stumbleupon.com>

---
+++ collectors/0/ifstat.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+# This file is part of tcollector.
+# Copyright (C) 2010  StumbleUpon, Inc.
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.  This program is distributed in the hope that it
+# will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+# General Public License for more details.  You should have received a copy
+# of the GNU Lesser General Public License along with this program.  If not,
+# see <http://www.gnu.org/licenses/>.
+#
+# network interface stats
+
+import os
+import sys
+import time
+import socket
+import re
+
+
+# /proc/net/dev has 16 fields, 8 for receive and 8 for xmit
+# The fields we care about are defined here.  The
+# ones we want to skip we just leave empty.
+# So we can aggregate up the total bytes, packets, etc
+# we tag each metric with direction=in or =out
+# and iface=
+
+FIELDS = ("bytes", "packets", "errs", "dropped",
+           None, None, None, None,)
+
+def main():
+    interval = 15
+
+    f = open("/proc/net/dev", "r")
+
+    # We just care about ethN interfaces.  We specifically
+    # want to avoid bond interfaces, because interface
+    # stats are still kept on the child interfaces when
+    # you bond.  By skipping bond we avoid double counting.
+    while True:
+        f.seek(0)
+        ts = int(time.time())
+        for line in f:
+            m = re.match("\s+(eth\d+):(.*)", line)
+            if not m:
+                continue
+            stats = m.group(2).split(None)
+            for i in range(8):
+                if FIELDS[i]:
+                    print ("proc.net.%s %d %s iface=%s direction=in"
+                           % (FIELDS[i], ts, stats[i], m.group(1)))
+                    print ("proc.net.%s %d %s iface=%s direction=out"
+                           % (FIELDS[i], ts, stats[i+8], m.group(1)))
+
+        sys.stdout.flush()
+        time.sleep(interval)
+
+if __name__ == "__main__":
+    main()
+
