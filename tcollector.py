8268f5b47b4334d11ca0b3b267d4d9cf4ae96d8d
Hugo Trippaers <hugo@strocamp.net>
2011-09-16 22:12:29 +0200

    IPv6 support: Use `getaddrinfo' to resolve the TSD's host.
    This way it works with IPv6 hosts and can work with DNS entries that
    have multiple A or AAAA records.
    
    Change-Id: I7f64ad730d98d732f8bef2f75576499e917a02bd
    Signed-off-by: Benoit Sigoure <tsunanet@gmail.com>

--- tcollector.py
+++ tcollector.py
@@ -521,16 +521,22 @@ class SenderThread(threading.Thread):
             LOG.debug('SenderThread blocking %0.2f seconds', try_delay)
             time.sleep(try_delay)
 
-            # now actually try the connection
-            try:
-                self.tsd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                self.tsd.settimeout(15)
-                self.tsd.connect((self.host, self.port))
-            except socket.error, msg:
-                LOG.error('failed to connect to %s:%d: %s',
-                           self.host, self.port, msg)
+            # Now actually try the connection.
+            adresses = socket.getaddrinfo(self.host, self.port, socket.AF_UNSPEC,
+                                          socket.SOCK_STREAM, 0)
+            for family, socktype, proto, canonname, sockaddr in adresses:
+                try:
+                    self.tsd = socket.socket(family, socktype, proto)
+                    self.tsd.settimeout(15)
+                    self.tsd.connect(sockaddr)
+                    # if we get here it connected
+                    break
+                except socket.error, msg:
+                    LOG.warning('Connection attempt failed to %s:%d: %s', host, port, msg)
                 self.tsd.close()
                 self.tsd = None
+            if not self.tsd:
+                LOG.error('Failed to connect to %s:%d', self.host, self.port)
 
     def send_data(self):
         """Sends outstanding data in self.sendq to the TSD in one operation."""
