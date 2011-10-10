0761fcd93d64b7f01e3eaaa1b66f58165a161e2b
Mark Smith <mark@qq.is>
2011-10-09 18:51:51 -0700

    Fixes and updates to Redis collector
    Implement a number of changes from feedback by Benoit.  This improves the readability of the code as well as functionality in a few places.  Additionally, add the evicted_keys data point.

--- collectors/0/redis-stats.py
+++ collectors/0/redis-stats.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python
 #
-# collector for Redis statistics.  this is very basic right now.  borrowed
-# somewhat from netstat.py.  we look for any running instances and try to
-# guess what port they're running on.
+# Copyright 2011 by Bump Technologies, Inc.
 #
-# please read the documentation string below for configuration information.
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.  This program is distributed in the hope that it
+# will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+# General Public License for more details.  You should have received a copy
+# of the GNU Lesser General Public License along with this program.  If not,
+# see <http://www.gnu.org/licenses/>.
 #
-# written by Mark Smith <mark@bu.mp>
+# Written by Mark Smith <mark@qq.is>.
 #
-
 
 """Statistics from a Redis instance.
 
@@ -31,6 +36,7 @@ This collector outputs the following metrics:
  - redis.connected_clients
  - redis.connected_slaves
  - redis.expired_keys
+ - redis.evicted_keys
  - redis.hash_max_zipmap_entries
  - redis.hash_max_zipmap_value
  - redis.keyspace_hits
@@ -54,14 +60,24 @@ For more information on these values, see this (not very useful) documentation:
 import os
 import pwd
 import re
-import redis
 import subprocess
 import sys
 import time
 
-# If we're running as root and this user exists, we'll drop privileges.  If this
-# is not set to 'root', that is.
+try:
+    import redis
+    has_redis = True
+except ImportError:
+    has_redis = False
+
+# If we are root, drop privileges to this user, if necessary.  NOTE: if this is
+# not root, this MUST be the user that you run redis-server under.  If not, we
+# will not be able to find your Redis instances.
 USER = "root"
+
+# Every SCAN_INTERVAL seconds, we look for new redis instances.  Prevents the
+# situation where you put up a new instance and we never notice.
+SCAN_INTERVAL = 300
 
 # these are the things in the info struct that we care about
 KEYS = [
@@ -69,7 +85,7 @@ KEYS = [
     'used_memory', 'total_commands_processed', 'used_memory_rss', 'total_connections_received', 'pubsub_patterns',
     'used_cpu_sys', 'blocked_clients', 'used_cpu_user', 'expired_keys', 'bgsave_in_progress', 'hash_max_zipmap_entries',
     'hash_max_zipmap_value', 'client_longest_output_list', 'client_biggest_input_buf', 'uptime_in_seconds',
-    'changes_since_last_save', 'mem_fragmentation_ratio', 'keyspace_hits'
+    'changes_since_last_save', 'mem_fragmentation_ratio', 'keyspace_hits', 'evicted_keys'
 ];
 
 def drop_privileges():
@@ -102,7 +118,11 @@ def main():
     last_scan = time.time()
     instances = scan_for_instances()  # port:name
     if not len(instances):
-        sys.exit(13)
+        return 13
+    if not has_redis:
+        sys.stderr.write("Found %d instance(s) to monitor, but the Python"
+                         " Redis module isn't installed.\n" % len(instances))
+        return 1
 
     def print_stat(metric, value, tags=""):
         if value is not None:
@@ -112,7 +132,7 @@ def main():
         ts = int(time.time())
 
         # if we haven't looked for redis instances recently, let's do that
-        if ts - last_scan > 300:
+        if ts - last_scan > SCAN_INTERVAL:
             instances = scan_for_instances()
             last_scan = ts
 
@@ -142,39 +162,39 @@ def scan_for_instances():
     figure out what configuration file they're using to name the cluster."""
 
     out = {}
-    tcre = re.compile(r"^\s*#\s*tcollector.(\w+)\s*=\s*(.+)$", re.IGNORECASE)
+    tcre = re.compile(r"^\s*#\s*tcollector.(\w+)\s*=\s*(.+)$")
 
     ns_proc = subprocess.Popen(["netstat", "-tnlp"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, _ = ns_proc.communicate()
-    if ns_proc.returncode == 0:
-        for line in stdout.split("\n"):
-            if not (line and 'redis-server' in line):
-                continue
-            pid = int(line.split()[6].split("/")[0])
-            port = int(line.split()[3].split(":")[1])
-
-            # now we have to get the command line.  we look in the redis config file for
-            # a special line that tells us what cluster this is.  else we default to using
-            # the port number which should work.
-            cluster = "port-%d" % port
-            try:
-                f = open("/proc/%d/cmdline" % pid)
-                cfg = f.readline().split("\0")[-2]
-                f.close()
-
-                f = open(cfg)
-                for cfgline in f:
-                    result = tcre.match(cfgline)
-                    if result:
-                        if result.group(1).lower() == "cluster":
-                            cluster = result.group(2).lower()
-            except:
-                # use the default cluster name if anything above failed.
-                pass
-
-            out[port] = cluster
-    else:
+    if ns_proc.returncode != 0:
         print >> sys.stderr, "failed to find instances %r" % ns_proc.returncode
+        return {}
+
+    for line in stdout.split("\n"):
+        if not (line and 'redis-server' in line):
+            continue
+        pid = int(line.split()[6].split("/")[0])
+        port = int(line.split()[3].split(":")[1])
+
+        # now we have to get the command line.  we look in the redis config file for
+        # a special line that tells us what cluster this is.  else we default to using
+        # the port number which should work.
+        cluster = "port-%d" % port
+        try:
+            f = open("/proc/%d/cmdline" % pid)
+            cfg = f.readline().split("\0")[-2]
+            f.close()
+
+            f = open(cfg)
+            for cfgline in f:
+                result = tcre.match(cfgline)
+                if result and result.group(1).lower() == "cluster":
+                    cluster = result.group(2).lower()
+        except EnvironmentError:
+            # use the default cluster name if anything above failed.
+            pass
+
+        out[port] = cluster
     return out
 
 
