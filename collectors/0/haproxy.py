f6d85c62f8c98e4baf2d3ba77fef544e46a66629
krausfm <frederik.kraus@gmail.com>
2011-12-19 17:18:59 +0100

    haproxy on steroids

--- collectors/0/haproxy.py
+++ collectors/0/haproxy.py
@@ -26,50 +26,96 @@ import re
 
 COLLECTION_INTERVAL = 10  # seconds
 
+BACKEND_SERVICES = { 'cluster_mysql_read' : 'mysql', 'static' : 'static', 'dynamic' : 'dynamic', 'varnish' : 'varnish' }
+FRONTEND_SERVICES = { 'images' : 'images', 'app' : 'app', 'mysql_read' : 'mysql', 'intern_dynamic' : 'varnish' }
+
 #HAPROXY_CSV_STAT_URL = 'file:///Users/fmk/Desktop/stats.csv'
 HAPROXY_CSV_STAT_URL = 'http://admin:GF123NET@62.146.7.79/admin/stats?;csv'
 
 def main():
     """haproxy main loop"""
     csvfile = None
-
+    
     while True:
         ts = int(time.time())
-
+        
         try:
             csvfile = urllib.urlopen(HAPROXY_CSV_STAT_URL);
-
+            
             reader = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONE)
             for row in reader:
-                if row[0] == 'app' and row[1] == 'FRONTEND':
-                    print ("haproxy.requests.total %d %s service=app" % (ts, row[48]))
 
-                    print ("haproxy.requests %d %s code=5xx service=app" % (ts, row[43]))
-                    print ("haproxy.requests %d %s code=4xx service=app" % (ts, row[42]))
-                    print ("haproxy.requests %d %s code=3xx service=app" % (ts, row[41]))
-                    print ("haproxy.requests %d %s code=2xx service=app" % (ts, row[40]))
-                    print ("haproxy.requests %d %s code=1xx service=app" % (ts, row[39]))
-
-
-                    print ("haproxy.session.total %d %s service=app" % (ts, row[7]))
-                    print ("haproxy.session.current %d %s service=app" % (ts, row[4]))
-
-                    print ("haproxy.session.max %d %s service=app" % (ts, row[5]))
                 
-                '''    
-                if row[0] == 'dynamic' and row[1] == 'BACKEND':
-                    print ("haproxy.queue.current %d %s cluster=app" % (ts, row[2]))
-                    print ("haproxy.queue.max %d %s cluster=app" % (ts, row[3]))
-                '''
+                """ collect all the frontend aggregates """
+                if row[0] in FRONTEND_SERVICES and row[1] == 'FRONTEND':
+                    
+                    """ only collect http info for http services """
+                    if FRONTEND_SERVICES[row[0]] is not 'mysql':
+                        
+                        print ("haproxy.requests.total %d %s service=%s interface=frontend" % (ts, row[48], FRONTEND_SERVICES[row[0]]))
+                    
+                        print ("haproxy.requests %d %s code=5xx service=%s" % (ts, row[43], FRONTEND_SERVICES[row[0]]))
+                        print ("haproxy.requests %d %s code=4xx service=%s" % (ts, row[42], FRONTEND_SERVICES[row[0]]))
+                        print ("haproxy.requests %d %s code=3xx service=%s" % (ts, row[41], FRONTEND_SERVICES[row[0]]))
+                        print ("haproxy.requests %d %s code=2xx service=%s" % (ts, row[40], FRONTEND_SERVICES[row[0]]))
+                        print ("haproxy.requests %d %s code=1xx service=%s" % (ts, row[39], FRONTEND_SERVICES[row[0]]))
+                    
+                    
+                    print ("haproxy.session.total %d %s service=%s" % (ts, row[7], FRONTEND_SERVICES[row[0]]))
+                    print ("haproxy.session.current %d %s service=%s" % (ts, row[4], FRONTEND_SERVICES[row[0]]))
+                    
+                    print ("haproxy.session.max %d %s service=%s" % (ts, row[5], FRONTEND_SERVICES[row[0]]))
+               
+                
+                """ collect global backend statistics """
+                if row[0] in BACKEND_SERVICES and row[1] == 'BACKEND':
+                    
+                    """ only collect http info for http services """
+                    if BACKEND_SERVICES[row[0]] is not 'mysql':
+                        
+                        print ("haproxy.backend.requests.total %d %s service=%s" % (ts, row[48], BACKEND_SERVICES[row[0]]))
+                        
+                        print ("haproxy.backend.requests %d %s code=5xx service=%s" % (ts, row[43], BACKEND_SERVICES[row[0]]))
+                        print ("haproxy.backend.requests %d %s code=4xx service=%s" % (ts, row[42], BACKEND_SERVICES[row[0]]))
+                        print ("haproxy.backend.requests %d %s code=3xx service=%s" % (ts, row[41], BACKEND_SERVICES[row[0]]))
+                        print ("haproxy.backend.requests %d %s code=2xx service=%s" % (ts, row[40], BACKEND_SERVICES[row[0]]))
+                        print ("haproxy.backend.requests %d %s code=1xx service=%s" % (ts, row[39], BACKEND_SERVICES[row[0]]))
+                    
+                    
+                    print ("haproxy.backend.session.total %d %s service=%s" % (ts, row[7], BACKEND_SERVICES[row[0]]))
+                    print ("haproxy.backend.session.current %d %s service=%s" % (ts, row[4], BACKEND_SERVICES[row[0]]))
+                    
+                    print ("haproxy.backend.session.max %d %s service=%s" % (ts, row[5], BACKEND_SERVICES[row[0]]))
 
+                elif row[0] in BACKEND_SERVICES and row[1] != 'BACKEND':
+                    
+                    """ only collect http info for http services """
+                    if BACKEND_SERVICES[row[0]] is not 'mysql':
+                        
+                        print ("haproxy.node.requests.total %d %s service=%s node=%s" % (ts, row[48], BACKEND_SERVICES[row[0]], row[1]))
+                        
+                        print ("haproxy.node.requests %d %s code=5xx service=%s node=%s" % (ts, row[43], BACKEND_SERVICES[row[0]], row[1]))
+                        print ("haproxy.node.requests %d %s code=4xx service=%s node=%s" % (ts, row[42], BACKEND_SERVICES[row[0]], row[1]))
+                        print ("haproxy.node.requests %d %s code=3xx service=%s node=%s" % (ts, row[41], BACKEND_SERVICES[row[0]], row[1]))
+                        print ("haproxy.node.requests %d %s code=2xx service=%s node=%s" % (ts, row[40], BACKEND_SERVICES[row[0]], row[1]))
+                        print ("haproxy.node.requests %d %s code=1xx service=%s node=%s" % (ts, row[39], BACKEND_SERVICES[row[0]], row[1]))
+                    
+                    
+                    print ("haproxy.node.session.total %d %s service=%s node=%s" % (ts, row[7], BACKEND_SERVICES[row[0]], row[1]))
+                    print ("haproxy.node.session.current %d %s service=%s node=%s" % (ts, row[4], BACKEND_SERVICES[row[0]], row[1]))
+                    
+                    print ("haproxy.node.session.max %d %s service=%s node=%s" % (ts, row[5], BACKEND_SERVICES[row[0]], row[1]))
+    
+                    print ("haproxy.node.downtime %d %s service=%s node=%s" % (ts, row[24], BACKEND_SERVICES[row[0]], row[1]))
+                                   
             csvfile.close()
-
+                 
         except socket.error, (errno, msg):
             print >> sys.stderr, "haproxy returned %r" % msg
             if csvfile is not None:
                 csvfile.close()
                 csvfile = None;
-
+                
         sys.stdout.flush()
         time.sleep(COLLECTION_INTERVAL)
 
