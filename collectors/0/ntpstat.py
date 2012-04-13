eaa2ba92587acbdd24970a6b60284cc97bfc54c5
Charles Gomes <charlesrg@gmail.com>
2012-04-13 19:02:51 -0400

    Gather an estimated time offset from ntp.
    This will return the offset estimated by ntpdc.
    It will not query the time server for timing information, it uses the localmachine estimated offset.

--- collectors/0/ntpstat.py
+++ collectors/0/ntpstat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
-# This file is part of tcollector.
-# Copyright (C) 2010  StumbleUpon, Inc.
+# NTP Offset stats
+# charlesrg AT gmail.com
 #
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU Lesser General Public License as published by
@@ -11,34 +11,11 @@
 # General Public License for more details.  You should have received a copy
 # of the GNU Lesser General Public License along with this program.  If not,
 # see <http://www.gnu.org/licenses/>.
-<<<<<<< HEAD
-"""df disk space and inode counts for TSDB """
-#
-# dfstat.py
-#
-# df.1kblocks.total      total size of fs
-# df.1kblocks.used       blocks used
-# df.1kblocks.available  blocks available
-# df.inodes.total        number of inodes
-# df.inodes.used        number of inodes
-# df.inodes.free        number of inodes
-
-# All metrics are tagged with mount= and fstype=
-# This makes it easier to exclude stuff like
-# tmpfs mounts from disk usage reports.
-
-# Because tsdb does not like slashes in tags, slashes will
-# be replaced by underscores in the mount= tag.  In theory
-# this could cause problems if you have a mountpoint of
-# "/foo/bar/" and "/foo_bar/".
-
-=======
 """ntp offset stats for TSDB """
 #
 # ntpstat.py
 #
 # ntp.offset             estimated offset
->>>>>>> ntp_collector
 
 import os
 import socket
@@ -52,41 +29,6 @@ COLLECTION_INTERVAL = 60  # seconds
 def main():
     """ntpstats main loop"""
 
-<<<<<<< HEAD
-    #find the primary server to get reference
-    #should be the prefer on ntp.conf or the first server if not preferred.
-    f = open('/etc/ntp.conf', 'r')
-    server="none"
-    for line in f:
-        fields = line.split()
-        if len(fields) <= 0 :
-            continue
-        if not line or fields[0] != 'server':
-            continue
-        if server == "none":
-            server=fields[1]
-        if line.find("prefer") > 0:
-            server=fields[1]
-    if server == "none":
-        print >> sys.stderr, "Could not find a suitable time server to query, please check your ntp.conf"
-
-    while True:
-        ts = int(time.time())
-	ntp_proc = subprocess.Popen(["ntpdate", "-q", server], stdout=subprocess.PIPE)
-        stdout, _ = ntp_proc.communicate()
-        if ntp_proc.returncode == 0:
-            for line in stdout.split("\n"): # pylint: disable=E1103
-                fields = line.split()
-                # skip header/blank lines
-                if not line or not fields[0].isdigit():
-                    continue
-
-                offset = fields[9]
-		server = fields[7]
-                print ("ntp.offset %d %s timeserver=%s"
-                       % (ts, offset, server ))
-                continue
-=======
     while True:
         ts = int(time.time())
 	ntp_proc = subprocess.Popen(["/usr/sbin/ntpdc", "-c", "loopinfo"], stdout=subprocess.PIPE)
@@ -103,7 +45,6 @@ def main():
                     continue
             print ("ntp.offset %d %s"
                     % (ts, offset))
->>>>>>> ntp_collector
         else:
             print >> sys.stderr, "ntpdate -q %s returned %r" % (server, ntp_proc.returncode)
 
