ee6abbfb6655edb5a3aa2b2e388c3d8267a894a3
Charles Gomes <charlesrg@gmail.com>
2012-04-13 15:24:24 -0400

    Added a new collector for NTP Offset. It parses your ntp.conf and find the most suitable server to check the Offset from.
    This will return the offset estimated by ntpdc.
    It will not query the time server for timing information, it uses the localmachine estimated offset.
    
    Update collectors/0/ntpstat.py
    
    Exits if not ntpdc found.
    Corrected error output
    Corrected output

---
+++ collectors/0/ntpstat.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+# NTP Offset stats
+# charlesrg AT gmail.com
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.  This program is distributed in the hope that it
+# will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+# General Public License for more details.  You should have received a copy
+# of the GNU Lesser General Public License along with this program.  If not,
+# see <http://www.gnu.org/licenses/>.
+"""ntp offset stats for TSDB """
+#
+# ntpstat.py
+#
+# ntp.offset             estimated offset
+
+import os
+import socket
+import subprocess
+import sys
+import time
+import errno
+
+
+COLLECTION_INTERVAL = 60  # seconds
+
+def main():
+    """ntpstats main loop"""
+
+    while True:
+        ts = int(time.time())
+        try:
+            ntp_proc = subprocess.Popen(["ntpdc", "-c", "loopinfo"], stdout=subprocess.PIPE)
+        except OSError, e:
+            if e.errno == errno.ENOENT:
+                # looks like ntpdc is not available, stop using this collector
+                sys.exit(13) # we signal tcollector to stop using this
+            raise
+
+        stdout, _ = ntp_proc.communicate()
+        if ntp_proc.returncode == 0:
+            for line in stdout.split("\n"):
+                if not line:
+                    continue
+                fields = line.split()
+                if len(fields) <= 0:
+                    continue
+                if fields[0] == "offset:":
+                    offset=fields[1]
+                    continue
+            print ("ntp.offset %d %s" % (ts, offset))
+        else:
+            print >> sys.stderr, "ntpdc -c loopinfo, returned %r" % (ntp_proc.returncode)
+
+        sys.stdout.flush()
+        time.sleep(COLLECTION_INTERVAL)
+
+if __name__ == "__main__":
+    main()
