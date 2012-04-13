f16da7d46154254c4b7771812a79f7c5815d6a00
Charles Gomes <charlesrg@gmail.com>
2012-04-13 15:24:24 -0400

    Added a new collector for NTP Offset. It parses your ntp.conf and find the most suitable server to check the Offset from.
    Just added and extra file with the NTP Offset collector.

---
+++ collectors/0/ntpstat.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+# This file is part of tcollector.
+# Copyright (C) 2010  StumbleUpon, Inc.
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.  This program is distributed in the hope that it
+# will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+# General Public License for more details.  You should have received a copy
+# of the GNU Lesser General Public License along with this program.  If not,
+# see <http://www.gnu.org/licenses/>.
+"""df disk space and inode counts for TSDB """
+#
+# dfstat.py
+#
+# df.1kblocks.total      total size of fs
+# df.1kblocks.used       blocks used
+# df.1kblocks.available  blocks available
+# df.inodes.total        number of inodes
+# df.inodes.used        number of inodes
+# df.inodes.free        number of inodes
+
+# All metrics are tagged with mount= and fstype=
+# This makes it easier to exclude stuff like
+# tmpfs mounts from disk usage reports.
+
+# Because tsdb does not like slashes in tags, slashes will
+# be replaced by underscores in the mount= tag.  In theory
+# this could cause problems if you have a mountpoint of
+# "/foo/bar/" and "/foo_bar/".
+
+
+import os
+import socket
+import subprocess
+import sys
+import time
+
+
+COLLECTION_INTERVAL = 60  # seconds
+
+def main():
+    """ntpstats main loop"""
+
+    #find the primary server to get reference
+    #should be the prefer on ntp.conf or the first server if not preferred.
+    f = open('/etc/ntp.conf', 'r')
+    server="none"
+    for line in f:
+        fields = line.split()
+        if len(fields) <= 0 :
+            continue
+        if not line or fields[0] != 'server':
+            continue
+        if server == "none":
+            server=fields[1]
+        if line.find("prefer") > 0:
+            server=fields[1]
+    if server == "none":
+        print >> sys.stderr, "Could not find a suitable time server to query, please check your ntp.conf"
+
+    while True:
+        ts = int(time.time())
+	ntp_proc = subprocess.Popen(["ntpdate", "-q", server], stdout=subprocess.PIPE)
+        stdout, _ = ntp_proc.communicate()
+        if ntp_proc.returncode == 0:
+            for line in stdout.split("\n"): # pylint: disable=E1103
+                fields = line.split()
+                # skip header/blank lines
+                if not line or not fields[0].isdigit():
+                    continue
+
+                offset = fields[9]
+		server = fields[7]
+                print ("ntp.offset %d %s timeserver=%s"
+                       % (ts, offset, server ))
+                continue
+        else:
+            print >> sys.stderr, "ntpdate -q %s returned %r" % (server, ntp_proc.returncode)
+
+        sys.stdout.flush()
+        time.sleep(COLLECTION_INTERVAL)
+
+if __name__ == "__main__":
+    main()
