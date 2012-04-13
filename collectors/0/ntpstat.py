bb8d6c629c48b26abf85a9ec88ed69e161d0ea37
Charles Gomes <charlesrg@gmail.com>
2012-04-13 18:51:24 -0400

    Gather an estimated time offset from ntp.
    This will return the offset estimated by ntpdc.
    It will not query the time server for timing information, it uses the localmachine estimated offset.

---
+++ collectors/0/ntpstat.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+# This file is part of tcollector.
+# Copyright (C) 2010  StumbleUpon, Inc.
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.  This program is distributed in the hope that it
+# will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+# General Public License for more details.  You should have received a copy
+# of the GNU Lesser General Public License along with this program.  If not,
+# see <http://www.gnu.org/licenses/>.
+"""ntp offset stats for TSDB """
+#
+# ntpstat.py
+#
+# ntp.offset             estimated offset
+
+import os
+import socket
+import subprocess
+import sys
+import time
+
+
+COLLECTION_INTERVAL = 60  # seconds
+
+def main():
+    """ntpstats main loop"""
+
+    while True:
+        ts = int(time.time())
+	ntp_proc = subprocess.Popen(["/usr/sbin/ntpdc", "-c", "loopinfo"], stdout=subprocess.PIPE)
+        stdout, _ = ntp_proc.communicate()
+        if ntp_proc.returncode == 0:
+            for line in stdout.split("\n"): 
+                if not line:
+                    continue
+                fields = line.split()
+                if len(fields) <= 0: 
+                    continue
+                if fields[0] == "offset:":
+                    offset=fields[1]    
+                    continue
+            print ("ntp.offset %d %s"
+                    % (ts, offset))
+        else:
+            print >> sys.stderr, "ntpdate -q %s returned %r" % (server, ntp_proc.returncode)
+
+        sys.stdout.flush()
+        time.sleep(COLLECTION_INTERVAL)
+
+if __name__ == "__main__":
+    main()
