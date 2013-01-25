e65e7957725273defe53976d8018047768ea3d84
Ion Savin <comp_@gmx.net>
2013-01-25 12:50:58 +0200

    Add collector for stats published by OpenTSDB over HTTP.

---
+++ collectors/0/opentsdb_stats.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Collects the OpenTSDB stats published over HTTP.
+
+URL="http://localhost:4242/stats"
+CURL_BIN="/usr/bin/curl"
+COLLECTION_INTERVAL="1m"
+
+while true
+do
+    ${CURL_BIN} ${URL}
+    sleep ${COLLECTION_INTERVAL}
+done
