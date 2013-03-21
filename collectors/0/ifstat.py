8604e10d860aeb0834b903e6c3d239f502458fb1
Benoit Sigoure <tsunanet@gmail.com>
2013-03-20 18:58:00 -0700

    Collect more stats from network interfaces.

--- collectors/0/ifstat.py
+++ collectors/0/ifstat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # This file is part of tcollector.
-# Copyright (C) 2010  StumbleUpon, Inc.
+# Copyright (C) 2010-2013  The tcollector Authors.
 #
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU Lesser General Public License as published by
@@ -22,15 +22,16 @@ import socket
 import re
 
 
-# /proc/net/dev has 16 fields, 8 for receive and 8 for xmit
-# The fields we care about are defined here.  The
-# ones we want to skip we just leave empty.
+# /proc/net/dev has 16 fields, 8 for receive and 8 for transmit,
+# defined below.
 # So we can aggregate up the total bytes, packets, etc
 # we tag each metric with direction=in or =out
 # and iface=
 
 FIELDS = ("bytes", "packets", "errs", "dropped",
-           None, None, None, None,)
+          "fifo.errs", "frame.errs", "compressed", "multicast",
+          "bytes", "packets", "errs", "dropped",
+          "fifo.errs", "collisions", "carrier.errs", "compressed")
 
 # If we're running as root and this user exists, we'll drop privileges.
 USER = "nobody"
@@ -67,17 +68,18 @@ def main():
             m = re.match("\s+(eth\d+):(.*)", line)
             if not m:
                 continue
+            intf = m.group(1)
             stats = m.group(2).split(None)
-            for i in range(8):
-                if FIELDS[i]:
-                    print ("proc.net.%s %d %s iface=%s direction=in"
-                           % (FIELDS[i], ts, stats[i], m.group(1)))
-                    print ("proc.net.%s %d %s iface=%s direction=out"
-                           % (FIELDS[i], ts, stats[i+8], m.group(1)))
+            def direction(i):
+                if i >= 8:
+                    return "out"
+                return "in"
+            for i in xrange(16):
+                print ("proc.net.%s %d %s iface=%s direction=%s"
+                       % (FIELDS[i], ts, stats[i], intf, direction(i)))
 
         sys.stdout.flush()
         time.sleep(interval)
 
 if __name__ == "__main__":
     main()
-
