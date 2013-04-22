4f0152428ee09038ecf4f5dba73ea8838f960a2c
Benoit Sigoure <tsunanet@gmail.com>
2013-04-21 22:33:17 -0700

    Don't die if we temporarily fail to resolve a DNS name.

--- tcollector.py
+++ tcollector.py
@@ -589,7 +589,7 @@ class SenderThread(threading.Thread):
                 addresses = socket.getaddrinfo(self.host, self.port, socket.AF_UNSPEC,
                                                socket.SOCK_STREAM, 0)
             except socket.gaierror, e:
-                if e[0] == socket.EAI_AGAIN:
+                if e[0] in (socket.EAI_AGAIN, EAI_NONAME):
                     continue
                 raise
             for family, socktype, proto, canonname, sockaddr in addresses:
