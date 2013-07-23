7b5659db0dfbc3915cf16f90e3fbf92e1deec1da
Benoit Sigoure <tsunanet@gmail.com>
2013-07-22 22:25:04 -0700

    Close file descriptors before exec'ing collectors.
    Without this we leak FD 3 to the children, which is typically used by
    the logging library to hold a reference to the current file (which
    will eventually get rotated and later possibly deleted).

--- tcollector.py
+++ tcollector.py
@@ -1131,7 +1131,9 @@ def spawn_collector(col):
     #     ... load the py module directly instead of using a subprocess ...
     try:
         col.proc = subprocess.Popen(col.filename, stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE, preexec_fn=os.setsid)
+                                    stderr=subprocess.PIPE,
+                                    close_fds=True,
+                                    preexec_fn=os.setsid)
     except OSError, e:
         LOG.error('Failed to spawn collector %s: %s' % (col.filename, e))
         return
