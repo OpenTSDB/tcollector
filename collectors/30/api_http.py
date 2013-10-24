483de885580935344fd5b4ea8eed5d0c516c5622
Mike Davis <mike@optimizely.com>
2013-10-24 10:36:15 -0700

    Add script to pull from api stats.

---
+++ collectors/30/api_http.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+import time
+import simplejson as json
+import requests
+import re
+ 
+ 
+# Constants
+METRIC_PREFIX = 'optimizely.api.'
+TIME = int(time.time())
+URL_FORMAT = 'http://127.0.0.1:8080/v1/{}/stats'
+
+KEYS = {
+    'opCount': "^(.*)Count$",
+    'opTime': "^(.*)Time$",
+    'opDuration': "^(.*)Duration$"
+}
+
+PATHS = [
+    "results"
+]
+ 
+def get_json(url):
+    """ Request URL, load JSON, exit if error. """
+    url_json = None
+    try:
+        r = requests.get(url)
+    except Exception, e:
+        print 'Unable to query url {} - {}'.format(url, e)
+        raise e
+    if r.status_code == 200:
+        try:
+            url_json = r.json()
+        except Exception, e:
+            print 'Could not load JSON for {}'.format(url)
+            raise e
+    else:
+        print 'Did not receive 200 response for {}'.format(url)
+    return url_json
+
+ 
+def format_tsd_key(metric_key, metric_value, tags={}):
+    """ Formats a key for OpenTSDB """
+    expanded_tags = ''.join([' %s=%s' % (key, value) for key, value in tags.iteritems()])
+    output = '{} {} {} {}'.format(metric_key, TIME, metric_value, expanded_tags)
+    return output
+ 
+ 
+def main():
+    # Collect statistics
+    for path in PATHS:
+        json = get_json(URL_FORMAT.format(path))
+
+        for item, value in json.iteritems():
+            for key, tag_expression in KEYS.iteritems():
+                matches = re.match(tag_expression, item)
+                if matches:
+                    tag = matches.group(1)
+                    print format_tsd_key(METRIC_PREFIX + path + "." + key, value, {'type': tag})
+ 
+ 
+if __name__ == '__main__':
+    main()
