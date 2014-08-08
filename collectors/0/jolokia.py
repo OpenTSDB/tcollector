202237058c9d6d5b926178faa2e43257ef8c3634
Mathieu Payeur Levallois <math.pay@gmail.com>
2014-08-08 16:22:40 -0400

    Deep copy of monitors is preferable since we modify the object.

--- collectors/0/jolokia.py
+++ collectors/0/jolokia.py
@@ -12,6 +12,7 @@
 
 import time
 import sys
+import copy
 try:
     import simplejson as json
 except ImportError:
@@ -64,7 +65,9 @@ class JolokiaCollector():
         self.j4p = Jolokia(self.url)
         self.j4p.auth(httpusername=self.auth['username'], httppassword=self.auth['password'])
         self.j4p.config(ignoreErrors=True)
-        for m in monitors:
+
+        self.monitors = deep.copy(monitors)
+        for m in self.monitors:
             if 'tags' in m:
                 m['tags'].update(tags)
             else:
@@ -77,11 +80,9 @@ class JolokiaCollector():
 
             # set a default not_tags to '' to catch empty tag keys and append
             # the value to metric name instead
-            if '' not in m['not_tags']:
-                m['not_tags'] += ['']
+            m['not_tags'] += ['']
 
             self.j4p.add_request(type='read', mbean=m['mbean'])
-        self.monitors = monitors
 
     def print_metrics(self, d, metric_prefix, timestamp, tags, not_tags=[]):
         """ Take a dict of attributes and print out numerical metric strings
