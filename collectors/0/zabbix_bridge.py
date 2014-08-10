30fb3468ac2381db2975111f47a524015ff0ee71
Chris Christensen <christianchristensen@gmail.com>
2014-08-10 17:10:29 -0500

    Zabbix MySQL replication: additional notes and begin item key lookup.
    [#MON-3138]

--- collectors/0/zabbix_bridge.py
+++ collectors/0/zabbix_bridge.py
@@ -4,20 +4,42 @@
 # Dump all replication item/metric insert events from a zabbix mysql server
 #
 
-from pymysqlreplication import BinLogStreamReader
-from pymysqlreplication.row_event import (
-    WriteRowsEvent
-)
+import re
+import sys
+
+try:
+    from pymysqlreplication import BinLogStreamReader
+    from pymysqlreplication.row_event import (
+        WriteRowsEvent
+    )
+except ImportError:
+    BinLogStreamReader = None  # This is handled gracefully in main()
+
+try:
+    import pymysql
+except ImportError:
+    pymysql = None # This is handled gracefully in main()
+
+# from collectors.etc import zabbix_bridge_conf
+from collectors.lib import utils
 
 MYSQL_SETTINGS = {
     'host': '127.0.0.1',
     'port': 3306,
     'user': '',
-    'passwd': ''
+    'passwd': '',
+    'db': 'zabbix'
 }
 
 
-def main():
+def main(args):
+    if BinLogStreamReader is None:
+        utils.err("error: Python module `pymysqlreplication' is missing")
+        return 1
+    if pymysql is None:
+        utils.err("error: Python module `pymysql' is missing")
+        return 1
+
     # server_id is your slave identifier, it should be unique.
     # set blocking to True if you want to block and wait for the next event at
     # the end of the stream
@@ -27,18 +49,21 @@ def main():
                                 blocking=True)
 
     for binlogevent in stream:
-        table = binlogevent.table
-        log_pos = binlogevent.packet.log_pos
-        if table == 'history' or table == 'history_uint':
-            for row in binlogevent.rows:
-                r = row['values']
-                print "pos:%s table:%s raw.%s %d %s" % (log_pos, table, r['itemid'], r['clock'], r['value'])
+        if binlogevent.schema == MYSQL_SETTINGS['db']:
+            table = binlogevent.table
+            log_pos = binlogevent.packet.log_pos
+            if table == 'history' or table == 'history_uint':
+                for row in binlogevent.rows:
+                    r = row['values']
+                    print "zbx.raw.%s %d %s log_pos=%s table=%s" % (r['itemid'], r['clock'], r['value'], log_pos, table)
+                sys.stdout.flush()
 
     stream.close()
 
 
 if __name__ == "__main__":
-    main()
+    sys.stdin.close()
+    sys.exit(main(sys.argv))
 
 
 ## Sample zabbix debug dump:
@@ -77,3 +102,12 @@ if __name__ == "__main__":
 # ('*', u'value', ':', 0.0254)
 # ('*', u'clock', ':', 1407124057)
 # ()
+
+## SQL item and host info
+# SELECT i.itemid, i.key_, h.host, h2.host FROM items i
+#  JOIN hosts h ON i.hostid=h.hostid
+#  LEFT JOIN hosts h2 ON h2.hostid=h.proxy_hostid;
+
+## Translation of item key_
+# Note: http://opentsdb.net/docs/build/html/user_guide/writing.html#metrics-and-tags
+# sub(/[^a-zA-Z0-9\-_\.]/,"_")
