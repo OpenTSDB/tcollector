321d9f2fe2061119884ecf1471a05779faa6ca6d
Neil Fulwiler <neil@optimizely.com>
2014-10-27 19:00:46 -0700

    exit 0 on failure to connect (not 13, we dont want to be removed!)

--- collectors/30/api_http.py
+++ collectors/30/api_http.py
@@ -4,14 +4,14 @@ import simplejson as json
 import requests
 import re
 import sys
- 
- 
+
+
 # Constants
 TIME = int(time.time())
 URL = 'http://127.0.0.1:8081/metrics'
 METRIC_PREFIX = 'optimizely.codahale.'
 
- 
+
 def get_json(url):
     """ Request URL, load JSON, exit if error. """
     url_json = None
@@ -19,7 +19,7 @@ def get_json(url):
         r = requests.get(url)
     except Exception, e:
         print 'Unable to query url {} - {}'.format(url, e)
-        sys.exit(13)
+        sys.exit(0)
     if r.status_code == 200:
         try:
             url_json = r.json()
@@ -30,17 +30,17 @@ def get_json(url):
         print 'Did not receive 200 response for {}'.format(url)
     return url_json
 
- 
+
 def format_tsd_key(metric_key, metric_value, tags={}):
     """ Formats a key for OpenTSDB """
     expanded_tags = ''.join([' %s=%s' % (key, value) for key, value in tags.iteritems()])
     output = '{} {} {} {}'.format(metric_key, TIME, metric_value, expanded_tags)
     return output
- 
- 
+
+
 def main():
     json = get_json(URL)
-    # We only have timers now - not sure if 
+    # We only have timers now - not sure if
     # the other ones look the same
     metric_types = ['timers', 'meters', 'counters', 'gauges']
     not_metrics = ['duration_units', 'rate_units', 'units']
@@ -57,7 +57,7 @@ def main():
                 print format_tsd_key(METRIC_PREFIX + metric_type + "." + metric, value, {
                                      'class': class_name,
                                      'method': method_name})
- 
- 
+
+
 if __name__ == '__main__':
     main()
