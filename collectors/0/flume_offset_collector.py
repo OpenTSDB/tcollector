00d06d3fc76ecc24c02326dc30432b34b013a6b2
Krishnan Chandra <krishnan@optimizely.com>
2015-08-26 17:15:31 -0700

    Add cluster ID to offset tags

--- collectors/0/flume_offset_collector.py
+++ collectors/0/flume_offset_collector.py
@@ -7,6 +7,7 @@ class FlumeOffsetCollector(KafkaOffsetCollector):
     KAFKA_CHROOT = ""
     ZK_QUORUM = ["zookeeperKafkaProd-1", "zookeeperKafkaProd-2", "zookeeperKafkaProd-3", "zookeeperKafkaProd-4", "zookeeperKafkaProd-5"]
     CONSUMER_GROUPS = {"flumeS3", "flumeHDFS", "flumeP13N"}
+    CLUSTER_NAME = "KafkaProd"
 
     def __init__(self):
         super(FlumeOffsetCollector, self).__init__()
