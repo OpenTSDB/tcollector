25699a1c19edbe97e5367876080d19a012adf1de
Jonathan Creasy <jonathan.creasy@gmail.com>
2016-02-05 18:09:52 -0600

    Tested, works for all metrics and namespaces available

--- collectors/0/aws_instances_stats.py
+++ collectors/0/aws_instances_stats.py
@@ -1,49 +1,101 @@
 #!/usr/bin/python
-
 import os
 import sys
 import time
 import datetime
+import json
 from time import mktime
 from collectors.etc import awsconf
 
 try:
     import boto.ec2
     from boto.ec2.cloudwatch import CloudWatchConnection
-    from boto.ec2.connection import EC2Connection
     from boto.ec2.cloudwatch import regions
 except ImportError:
-    boto = None
+    exit(13)
 
-SCAN_INTERVAL = 300
-COLLECTION_INTERVAL = 15
+COLLECTION_INTERVAL = 300
 
 STATISTICS = frozenset([
                         'Minimum',
                         'Maximum',
                         'Average',
+                        'Sum',
+                        'SampleCount'
                        ])
 
-def err(e):
-    print >> sys.stderr, e
-
-def cloudwatch_connect():
-    access_key, secret_access_key = awsconf.get_accesskey_secretkey()
-    return CloudWatchConnection(aws_access_key_id=access_key, aws_secret_access_key=secret_access_key)
+NAMESPACES = frozenset([
+                        'AWS/AutoScaling',
+                        'AWS/Billing',
+                        'AWS/CloudFront',
+                        'AWS/CloudSearch',
+                        'AWS/Events',
+                        'AWS/DynamoDB',
+                        'AWS/ECS',
+                        'AWS/ElastiCache',
+                        'AWS/EBS',
+                        'AWS/EC2',
+                        'AWS/ELB',
+                        'AWS/ElasticMapReduce',
+                        'AWS/ES',
+                        'AWS/Kinesis',
+                        'AWS/Lambda',
+                        'AWS/ML',
+                        'AWS/OpsWorks',
+                        'AWS/Redshift',
+                        'AWS/RDS',
+                        'AWS/Route53',
+                        'AWS/SNS',
+                        'AWS/SQS',
+                        'AWS/S3',
+                        'AWS/SWF',
+                        'AWS/StorageGateway',
+                        'AWS/WAF',
+                        'AWS/WorkSpaces'
+                       ])
 
 def cloudwatch_connect_to_region(region):
     access_key, secret_access_key = awsconf.get_accesskey_secretkey()
-    return boto.ec2.cloudwatch.connect_to_region(region, aws_access_key_id=access_key, aws_secret_access_key=secret_access_key)
+    try:
+        conn =  boto.ec2.cloudwatch.connect_to_region(region, aws_access_key_id=access_key, aws_secret_access_key=secret_access_key)
+    except:
+        print "Unexpected error:", sys.exc_info()[0]
+    else:
+        return conn
 
 def cloudwatch_list_metrics(conn):
-    cloudwatch_metrics = []
-    for i in conn.list_metrics():
-        m = str(i).split(":")[1]
-        if m in cloudwatch_metrics:
-            continue
-        else:
-            cloudwatch_metrics.append(m)
-    return cloudwatch_metrics
+    return conn.list_metrics()
+
+def cloudwatch_query_metric(metric, statistic):
+    end = datetime.datetime.utcnow()
+    start = end - datetime.timedelta(seconds=COLLECTION_INTERVAL)
+    tags = metric.dimensions
+    datapoints = metric.query(start, end, statistic)
+    if len(datapoints) > 0:
+        for datapoint in datapoints:
+            timestamp = str(datapoint['Timestamp'])
+            st = time.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
+            dt = datetime.datetime.fromtimestamp(mktime(st))
+            timestamp = dt.strftime("%s")
+            value = datapoint[statistic]
+            taglist = ""
+
+            for tagk,tagv in tags.iteritems():
+                tagkey = str(tagk).lower()
+                tagval = str(tagv[0]).lower()
+                taglist = "%s %s=%s" % (taglist, tagkey, tagval)
+
+            metric_name = metric.name.lower()
+
+            if metric_name == 'networkout':
+                metric_name = 'network'
+                taglist = "%s %s=%s" % (taglist, 'direction', 'out')
+
+            if metric_name == 'networkin':
+                metric_name = 'network'
+                taglist = "%s %s=%s" % (taglist, 'direction', 'in')
+
+            print "%s.%s.%s %s %s %s" % (metric.namespace.lower().replace('/','.'), metric_name, statistic.lower(), str(timestamp), str(value), taglist)
 
 def ec2_connect_to_region(region):
     access_key, secret_access_key = awsconf.get_accesskey_secretkey()
@@ -51,93 +103,19 @@ def ec2_connect_to_region(region):
 
 def ec2_list_regions():
     ec2_regions = []
-    for i in regions():
+    for i in boto.ec2.cloudwatch.regions():
         ec2_regions.append(str(i.name))
     return ec2_regions
 
-def ec2_list_instances(conn):
-    instances = {}
-    instances_regions = {}
-    reservations = conn.get_all_reservations()
-    for r in reservations:
-        for inst in r.instances:
-            if str(inst.state) == "running":
-                instances[str(inst.id)] = str(inst.placement)
-                instances_regions[str(inst.id)] = str(inst.region.name)
-    return (instances,instances_regions)
-
-def print_stat(metric, ts, value, tags=""):
-    if value is not None:
-        print "aws.%s %s %s %s" % (metric.lower(), ts, value, tags)
-
 def main():
-    instances = {}
-    instances_regions = {}
-    cloudwatch_conn = {}
-    last_scan = time.time()
-
-    # List all regions
-    regions = ec2_list_regions()
-
-    for reg in regions:
-        # Request for us-gov-west-1 AWS Region (esp. for U.S. Govt Agencies) throws invalid permission
-        if reg != "us-gov-west-1":
-            ec2_conn = ec2_connect_to_region(reg)
-            inst, inst_reg = ec2_list_instances(ec2_conn)
-            instances.update(inst)
-            instances_regions.update(inst_reg)
-
-    # Connect to each region's cloudwatch to collect metrics of that region
-    for reg in regions:
-        # Request for us-gov-west-1 AWS Region (esp. for U.S. Govt Agencies) throws invalid permission
-        if reg != "us-gov-west-1":
-            cloudwatch_conn[reg] = cloudwatch_connect_to_region(reg)
-
-    # Create CloudWatchConnection
-    try:
-        cw_conn = cloudwatch_connect()
-    except:
-        err("Can't connect to CloudWatch with these credentials")
-        return 13   # Ask tcollector not to respawn us
-
-    cw_metrics = cloudwatch_list_metrics(cw_conn)
-
     while True:
-        ts = time.time()
-
-        # We haven't looked for any new ec2 instance recently, let's do that
-        if ts - last_scan > SCAN_INTERVAL:
-            for reg in regions:
-                if reg != "us-gov-west-1":
-                    ec2_conn = ec2_connect_to_region(reg)
-                    inst, inst_reg = ec2_list_instances(ec2_conn)
-                    instances.update(inst)
-                    instances_regions.update(inst_reg)
-                    last_scan = ts
-
-        if not instances:
-            return 13   # Ask tcollector not to respawn us
-
-        # Start Time
-        start = datetime.datetime.utcnow() - datetime.timedelta(seconds=120)
-
-        # End Time
-        end = datetime.datetime.utcnow()
-
-        # Iterate for each instance and collect datapoints
-        for instance,placement in instances.iteritems():
-            for metric in cw_metrics:
+        regions = ec2_list_regions()
+        for reg in regions:
+            region_conn = cloudwatch_connect_to_region(reg)
+            metrics = cloudwatch_list_metrics(region_conn)
+            for metric in metrics:
                 for statistic in STATISTICS:
-                    stats = cloudwatch_conn[instances_regions[instance]].get_metric_statistics(60, start, end, metric, 'AWS/EC2', statistic, dimensions={'InstanceId':[instance]})
-                    if stats:
-                        timestamp = str(stats[0]['Timestamp'])
-                        st = time.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
-                        dt = datetime.datetime.fromtimestamp(mktime(st))
-                        timestamp = dt.strftime("%s")
-                        value = stats[0][statistic]
-                        tags = "instance-id=%s statistic=%s placement=%s" % (instance, statistic, placement)
-                        print_stat(metric, timestamp, value, tags)
-
+                    cloudwatch_query_metric(metric, statistic)
         time.sleep(COLLECTION_INTERVAL)
 
 if __name__ == "__main__":
