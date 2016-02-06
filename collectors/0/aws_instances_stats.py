f86cd2b7cd4ea123dad23d0be7ddd8957200ef83
Jonathan Creasy <jonathan.creasy@gmail.com>
2016-02-05 18:13:18 -0600

    Remove extra set

--- collectors/0/aws_instances_stats.py
+++ collectors/0/aws_instances_stats.py
@@ -24,36 +24,6 @@ STATISTICS = frozenset([
                         'SampleCount'
                        ])
 
-NAMESPACES = frozenset([
-                        'AWS/AutoScaling',
-                        'AWS/Billing',
-                        'AWS/CloudFront',
-                        'AWS/CloudSearch',
-                        'AWS/Events',
-                        'AWS/DynamoDB',
-                        'AWS/ECS',
-                        'AWS/ElastiCache',
-                        'AWS/EBS',
-                        'AWS/EC2',
-                        'AWS/ELB',
-                        'AWS/ElasticMapReduce',
-                        'AWS/ES',
-                        'AWS/Kinesis',
-                        'AWS/Lambda',
-                        'AWS/ML',
-                        'AWS/OpsWorks',
-                        'AWS/Redshift',
-                        'AWS/RDS',
-                        'AWS/Route53',
-                        'AWS/SNS',
-                        'AWS/SQS',
-                        'AWS/S3',
-                        'AWS/SWF',
-                        'AWS/StorageGateway',
-                        'AWS/WAF',
-                        'AWS/WorkSpaces'
-                       ])
-
 def cloudwatch_connect_to_region(region):
     access_key, secret_access_key = awsconf.get_accesskey_secretkey()
     try:
