4873da4685b945ce543875da6f52ed5a5e077fbe
benrubson <ben.rubson@gmail.com>
2016-02-09 21:16:29 +0100

    detailed CPU statistics

---
+++ collectors/0/cpu_detailed.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# This file is part of tcollector.
+# Copyright (C) 2012  The tcollector Authors.
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.  This program is distributed in the hope that it
+# will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+# General Public License for more details.  You should have received a copy
+# of the GNU Lesser General Public License along with this program.  If not,
+# see <http://www.gnu.org/licenses/>.
+#
+
+import errno
+import sys
+import time
+import subprocess
+import re
+import signal
+import os
+
+
+'''
+CPU detailed statistics for TSDB
+
+This plugin tracks, for all CPUs:
+
+- user %
+- nice %
+- system %
+- interrupt %
+- idle %
+'''
+
+signal_received = None
+def handlesignal(signum, stack):
+    global signal_received
+    signal_received = signum
+
+def main():
+    """top main loop"""
+    global signal_received
+
+    signal.signal(signal.SIGTERM, handlesignal)
+    signal.signal(signal.SIGINT, handlesignal)
+
+    try:
+        p_top = subprocess.Popen(
+            ["top", "-t", "-I", "-P", "-n", "-s15", "-d40320"],
+            stdout=subprocess.PIPE,
+        )
+    except OSError, e:
+        if e.errno == errno.ENOENT:
+            # it makes no sense to run this collector here
+            sys.exit(13) # we signal tcollector to not run us
+        raise
+
+    while signal_received is None:
+        try:
+            line = p_top.stdout.readline()
+        except (IOError, OSError), e:
+            if e.errno in (errno.EINTR, errno.EAGAIN):
+                break
+            raise
+
+        if not line:
+            # end of the program, die
+            break
+
+        fields = line.split()
+        if len(fields) <= 0:
+            continue
+
+        if fields[0] == "CPU":
+            cpuid=fields[1][:-1]
+            cpuuser=fields[2][:-1]
+            cpunice=fields[4][:-1]
+            cpusystem=fields[6][:-1]
+            cpuinterrupt=fields[8][:-1]
+            cpuidle=fields[10][:-1]
+            print ("cpu.%s.user %s" % (cpuid, cpuuser))
+            print ("cpu.%s.nice %s" % (cpuid, cpunice))
+            print ("cpu.%s.system %s" % (cpuid, cpusystem))
+            print ("cpu.%s.interrupt %s" % (cpuid, cpuinterrupt))
+            print ("cpu.%s.idle %s" % (cpuid, cpuidle))
+
+    if signal_received is None:
+        signal_received = signal.SIGTERM
+    try:
+        os.kill(p_top.pid, signal_received)
+    except Exception:
+        pass
+    p_top.wait()
+
+if __name__ == "__main__":
+    main()
+
