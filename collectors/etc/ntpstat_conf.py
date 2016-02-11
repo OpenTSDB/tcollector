35f2cd53906f5f96331f34c0d7374ddef5cafd0f
benrubson <ben.rubson@gmail.com>
2016-02-11 11:03:41 +0100

    give ntpstat an optional configuration file

--- collectors/etc/ntpstat_conf.py
+++ collectors/etc/ntpstat_conf.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
 
-def enabled():
-  return True
+def get_config():
+
+    config = {
+        'collection_interval': 60    # Seconds, how often to collect metric data
+    }
+
+    return config
