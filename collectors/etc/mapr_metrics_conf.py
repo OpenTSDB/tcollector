1298bd022739fc343eb9503bb5efec874401d957
Jonathan Creasy <jonathan.creasy@gmail.com>
2016-02-15 21:43:53 -0600

    Disable mapr_metrics by default
    Fixes #196

--- collectors/etc/mapr_metrics_conf.py
+++ collectors/etc/mapr_metrics_conf.py
@@ -1,7 +1,7 @@
 # Before you enable the mapr_metrics collector, create the metrics:
 # tsdb mkmetric mapr.disks.READBYTES mapr.disks.WRITEOPS mapr.disks.WRITEBYTES mapr.disks.READOPS mapr.cpus.CPUTOTAL mapr.cpus.CPUIDLE mapr.cpus.CPUIOWAIT mapr.network.PKTSOUT mapr.network.BYTESOUT mapr.network.PKTSIN mapr.network.BYTESIN mapr.memory.used mapr.mfs.available mapr.mfs.used
 #
-enabled = True
+enabled = False
 webserver = "localhost"
 port = 8443
 no_ssl = False
