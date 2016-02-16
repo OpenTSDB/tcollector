1b2b55ac0785f4f6079e991e8f04ad6201450d9b
Jonathan Creasy <jonathan.creasy@gmail.com>
2016-02-16 08:07:45 -0600

    Forgot to include the configuration file

--- collectors/etc/docker_conf.py
+++ collectors/etc/docker_conf.py
@@ -12,6 +12,9 @@
 # of the GNU Lesser General Public License along with this program.  If not,
 # see <http://www.gnu.org/licenses/>.
 
+def enabled():
+  return False
+
 def get_config():
   config = {
     'interval': 15,
