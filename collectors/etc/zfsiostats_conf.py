bd45a3ba764470f12b6965a173cf6b30dfa08b41
benrubson <ben.rubson@gmail.com>
2016-02-25 20:17:16 +0100

    do not report capacity info too frequently

--- collectors/etc/zfsiostats_conf.py
+++ collectors/etc/zfsiostats_conf.py
@@ -3,7 +3,8 @@
 def get_config():
 
     config = {
-        'collection_interval': 15    # Seconds, how often to collect metric data
+        'collection_interval': 15,             # Seconds, how often to collect metric data
+        'report_capacity_every_x_times': 20    # Avoid reporting capacity info too frequently, 0 disables capacity reporting
     }
 
     return config
