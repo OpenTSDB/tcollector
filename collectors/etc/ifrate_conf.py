44162db9952d3a6f9c814ed3408d3f2b6e123bf3
benrubson <ben.rubson@gmail.com>
2016-03-05 01:07:23 +0100

    Common ifnames

--- collectors/etc/ifrate_conf.py
+++ collectors/etc/ifrate_conf.py
@@ -3,8 +3,8 @@
 def get_config():
 
     config = {
-        'collection_interval': 15,           # Seconds, how often to collect metric data
-	'interfaces': ['mlxen0','mlxen1']    # Interfaces to monitor
+        'collection_interval': 15,     # Seconds, how often to collect metric data
+	'interfaces': ['ix0','ix1']    # Interfaces to monitor
     }
 
     return config
