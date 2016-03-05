75899d7504af521f3755a9b4cbc994217bf4caa2
benrubson <ben.rubson@gmail.com>
2016-03-05 08:53:44 +0100

    typo

--- collectors/etc/ifrate_conf.py
+++ collectors/etc/ifrate_conf.py
@@ -4,7 +4,7 @@ def get_config():
 
     config = {
         'collection_interval': 15,     # Seconds, how often to collect metric data
-	'interfaces': ['ix0','ix1']    # Interfaces to monitor
+        'interfaces': ['ix0','ix1']    # Interfaces to monitor
     }
 
     return config
