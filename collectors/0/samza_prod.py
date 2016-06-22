4f28ca0d4f62f00c0d8db69281a0a691b11bf133
David Yu <david.yu@optimizely.com>
2016-06-22 15:11:43 -0700

    Report sessionization metrics to OpenTSDB

---
+++ collectors/0/samza_prod.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import sys
+
+from collectors.lib.samza_metric_reporter import SamzaMetricReporter
+
+CONSUMER_GROUP_ID = "tcollector_samza_prod"
+
+KAFKA_BOOTSTRAP_SERVERS = [
+    'kafkaprod-1:9092',
+    'kafkaprod-2:9092',
+    'kafkaprod-3:9092',
+    'kafkaprod-4:9092',
+    'kafkaprod-5:9092',
+    'kafkaprod-6:9092'
+]
+
+def main():
+
+    reporter = SamzaMetricReporter(CONSUMER_GROUP_ID, KAFKA_BOOTSTRAP_SERVERS)
+    reporter.run()
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ collectors/0/sessionization_staging.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import sys
+
+from collectors.lib.sessionization_metric_reporter import SessionizationMetricReporter
+
+CONSUMER_GROUP_ID = "tcollector_sessionization_staging"
+
+KAFKA_BOOTSTRAP_SERVERS = [
+    "kafkaStaging-1:9092",
+    "kafkaStaging-2:9092"
+]
+
+
+def main():
+
+    # FIXME: Add prod reporter
+
+    reporter = SessionizationMetricReporter(CONSUMER_GROUP_ID, KAFKA_BOOTSTRAP_SERVERS)
+    reporter.run()
+
+if __name__ == "__main__":
+    sys.exit(main())
