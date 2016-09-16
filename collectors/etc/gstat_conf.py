42d3758523de187460eef01350bbca020c3253e0
benrubson <ben.rubson@gmail.com>
2016-09-16 17:17:15 +0200

    remove test fields

--- collectors/etc/gstat_conf.py
+++ collectors/etc/gstat_conf.py
@@ -3,8 +3,8 @@
 def get_config():
 
     config = {
-        'collection_interval': 1,   # Seconds, how often to collect metric data
-        'collection_filter': 'G2203NHGD5ANYhm'    # Filter to choose disks, .* will take all disks
+        'collection_interval': 15,   # Seconds, how often to collect metric data
+        'collection_filter': '.*'    # Filter to choose disks, .* will take all disks
     }
 
     return config
