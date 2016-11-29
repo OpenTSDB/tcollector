9649a5e59ddfab44cb05c5ae7d53d07bdb0ac633
benrubson <ben.rubson@gmail.com>
2016-11-29 22:05:11 +0100

    Typo

--- collectors/etc/sysload_conf.py
+++ collectors/etc/sysload_conf.py
@@ -4,7 +4,7 @@ def get_config():
 
     config = {
         'collection_interval': 15,    # Seconds, how often to collect metric data
-        'collect_every_cpu': True     # 1 will collect statistics for every CPU, 0 for the "ALL" CPU
+        'collect_every_cpu': True     # True will collect statistics for every CPU, False for the "ALL" CPU
     }
 
     return config
