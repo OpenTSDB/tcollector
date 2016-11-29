4938f9b34e445696e2d97e679c11f2881d5cf2a6
Yalei Wang <wangy1931@gmail.com>
2016-11-28 23:15:17 -0800

    improve tomcat specific deploy scripts to deal with multiple instances

--- deploy/collectors/tomcat/deploy.sh
+++ deploy/collectors/tomcat/deploy.sh
@@ -1,12 +1,39 @@
 #!/bin/bash
 
-if [[ "$#" -lt 1 ]]; then
-    echo "missing tomcat webapp path"
+conf_file="/opt/cloudwiz-agent/agent/collectors/conf/tomcat.conf"
+
+function display_usage() {
+    printf "usage:\n$0 tomcat-webapp1-root:port1 tomcat-webapp2-root:port2 ...\n"
+}
+
+if [[ "$#" -lt 1 ]]  || [[ $1 = '-h' ]]; then
+    display_usage
     exit 1
 fi
 
-tomcat_webapp_path=$1
 currdir=$(cd $(dirname $0); pwd)
-echo "cp $currdir/jolokia.war $tomcat_webapp_path"
-yes | cp -f $currdir/jolokia.war $tomcat_webapp_path
+ports=''
+for webapp_path_and_root in "$@"; do
+    echo "path_port=$webapp_path_and_root"
+    IFS=':'
+    path_port=(${webapp_path_and_root})
+    if [ -z "${path_port[0]}" ]; then
+        printf "${color_red}empty webapp root path. abort!${color_normal}\n"
+        exit 1
+    fi
+    if [ -z "${path_port[1]}" ]; then
+        printf "${color_red}empty port number. abort!${color_normal}\n"
+        exit 1
+    fi
+    echo "cp -f $currdir/jolokia.war ${path_port[0]}"
+    yes | cp -f $currdir/jolokia.war ${path_port[0]} || exit 1
+    echo ${path_port[1]}
+    ports="${ports},${path_port[1]}"
+done
 
+echo $ports
+ports=${ports:1}
+echo "set ports: $ports in $conf_file"
+if [[ -f $conf_file ]]; then
+    sed -i "/^ports *= */c\ports = $ports" "$conf_file"
+fi
