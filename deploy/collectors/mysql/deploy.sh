acb2931f7f6a2f14add77f0ff69821da36ef6391
Yalei Wang <wangy1931@gmail.com>
2016-12-06 12:05:31 -0800

    local copy to sql-python lib

--- deploy/collectors/mysql/deploy.sh
+++ deploy/collectors/mysql/deploy.sh
@@ -8,7 +8,11 @@ agent_install_folder="/opt/cloudwiz-agent"
 mysql_conf_file=${agent_install_folder}/agent/collectors/conf/mysql.conf
 mysql_stats_user="cloudwiz_user"
 mysql_stats_pass="cloudwiz_pass"
-mysql_priv_user=${1:-"root"}
+mysql_priv_user="root"
+
+function display_usage() {
+ log_info "$0 [-u privilege_user, e.g. user to connec to mysql, default is root] [-l localpath/to/MySQL-python-1.2.5.zip]"
+}
 
 function check_root() {
   if [[ "$USER" != "root" ]]; then
@@ -28,13 +32,34 @@ function log_info() {
   printf "${color_green}$1${color_normal}\n"
 }
 
+while getopts ":hu:l:" opt; do
+  case $opt in
+    h)
+        display_usage
+        exit 0
+        ;;
+    u)
+      mysql_priv_user=$OPTARG
+      ;;
+    l)
+      libpath=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      display_usage
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      display_usage
+      exit 1
+      ;;
+  esac
+done
+
 read -p "the mysql collector will create a mysql user called ${mysql_stats_user}. If you have a user in use with the same name, abort now and contact cloudwiz support person. Otherwise, press any key to continue."
 
 check_root
-
-if [ "$#" -lt 1 ]; then
-    log_info "no pass-in privilege user, default to ${mysql_priv_user}"
-fi 
 
 if [ ! -d "$agent_install_folder" ]; then
     printf "${color_red}cloudwiz-agent installation folder ($agent_install_folder) does not exist. abort!${color_normal}\n"
@@ -54,15 +79,21 @@ mysql -u "$mysql_priv_user" -p -e "GRANT USAGE ON *.* TO '${mysql_stats_user}'@'
 abort_if_failed "failed to create stats user '${mysql_stats_user}'@'localhost'"
 
 log_info "config mysql stats user/pass in ${mysql_conf_file} s/user:.*/user: ${mysql_stats_user}/g"
-sed -i.bak -e "s/enabled:.*/enabled: True/g" -e "s/user:.*/user: ${mysql_stats_user}/g" -e "s/pass:.*/pass: ${mysql_stats_pass}/g" ${mysql_conf_file}
+sed -i -e "s/enabled:.*/enabled: True/g" -e "s/user:.*/user: ${mysql_stats_user}/g" -e "s/pass:.*/pass: ${mysql_stats_pass}/g" ${mysql_conf_file}
 abort_if_failed "failed to config mysql collector conf file"
 
 if ! $("$agent_install_folder/altenv/bin/python" -c 'import MySQLdb' &> /dev/null); then
     log_info "install MySQLdb python module to talk to mysql to collect data"
     pushd "$agent_install_folder"
     mkdir -p workspace
-    wget --directory-prefix=./workspace/ https://pypi.python.org/packages/a5/e9/51b544da85a36a68debe7a7091f068d802fc515a3a202652828c73453cad/MySQL-python-1.2.5.zip#md5=654f75b302db6ed8dc5a898c625e030c
-    abort_if_failed 'failed to download https://pypi.python.org/packages/a5/e9/51b544da85a36a68debe7a7091f068d802fc515a3a202652828c73453cad/MySQL-python-1.2.5.zip#md5=654f75b302db6ed8dc5a898c625e030c'
+    if [ -z "$libpath" ]; then
+        wget --directory-prefix=./workspace/ https://pypi.python.org/packages/a5/e9/51b544da85a36a68debe7a7091f068d802fc515a3a202652828c73453cad/MySQL-python-1.2.5.zip#md5=654f75b302db6ed8dc5a898c625e030c
+        abort_if_failed 'failed to download https://pypi.python.org/packages/a5/e9/51b544da85a36a68debe7a7091f068d802fc515a3a202652828c73453cad/MySQL-python-1.2.5.zip#md5=654f75b302db6ed8dc5a898c625e030c'
+    else
+        log_info "cp $libpath workspace/MySQL-python-1.2.5.zip"
+        cp "$libpath" workspace/MySQL-python-1.2.5.zip
+        abort_if_failed "failed to cp $libpath workspace/MySQL-python-1.2.5.zip"
+    fi
     unzip workspace/MySQL-python-1.2.5.zip -d workspace/
     abort_if_failed 'failed to unzip workspace/MySQL-python-1.2.5.zip'
     cd workspace/MySQL-python-1.2.5
