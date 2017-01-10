0e646507632695e46693c77d0afca791df0a0f80
hehaiyuan <hehaiyuan.hhy@gmail.com>
2017-01-10 19:11:31 +0800

    add try/catch in call method

--- collectors/builtin/cpus_pctusage.py
+++ collectors/builtin/cpus_pctusage.py
@@ -55,32 +55,35 @@ class CpusPctusage(CollectorBase):
                 )
         except OSError:
             self._readq.nput("cpu.state %s %s" % (int(time.time()), '1'))
-            self.log_error("cpus_pctusage collector except error, abort")
+            self.log_error("cpus_pctusage collector except error, abort %s" % OSError)
             return
         self.log_info('CpusPctusage created subprocess %d', self.p_top.pid)
 
     def __call__(self):
+        try:
+            while not self._exit:
+                line = self.p_top.stdout.readline()
+                fields = re.sub(r"%( [uni][a-z]+,?)? | AM | PM ", "", line).split()
+                if len(fields) <= 0:
+                    continue
 
-        while not self._exit:
-            line = self.p_top.stdout.readline()
-            fields = re.sub(r"%( [uni][a-z]+,?)? | AM | PM ", "", line).split()
-            if len(fields) <= 0:
-                continue
-
-            if (((fields[0] == "CPU") or (re.match("[0-9][0-9]:[0-9][0-9]:[0-9][0-9]",fields[0]))) and (re.match("[0-9]+:?",fields[1]))):
-                timestamp = int(time.time())
-                cpuid = fields[1].replace(":","")
-                cpuuser = fields[2]
-                cpunice = fields[3]
-                cpusystem = fields[4]
-                cpuinterrupt = fields[6]
-                cpuidle = fields[-1]
-                self._readq.nput("cpu.usr %s %s cpu=%s" % (timestamp, cpuuser, cpuid))
-                self._readq.nput("cpu.nice %s %s cpu=%s" % (timestamp, cpunice, cpuid))
-                self._readq.nput("cpu.sys %s %s cpu=%s" % (timestamp, cpusystem, cpuid))
-                self._readq.nput("cpu.irq %s %s cpu=%s" % (timestamp, cpuinterrupt, cpuid))
-                self._readq.nput("cpu.idle %s %s cpu=%s" % (timestamp, cpuidle, cpuid))
-                self._readq.nput("cpu.state %s %s" % (int(time.time()), '0'))
+                if (((fields[0] == "CPU") or (re.match("[0-9][0-9]:[0-9][0-9]:[0-9][0-9]",fields[0]))) and (re.match("[0-9]+:?",fields[1]))):
+                    timestamp = int(time.time())
+                    cpuid = fields[1].replace(":","")
+                    cpuuser = fields[2]
+                    cpunice = fields[3]
+                    cpusystem = fields[4]
+                    cpuinterrupt = fields[6]
+                    cpuidle = fields[-1]
+                    self._readq.nput("cpu.usr %s %s cpu=%s" % (timestamp, cpuuser, cpuid))
+                    self._readq.nput("cpu.nice %s %s cpu=%s" % (timestamp, cpunice, cpuid))
+                    self._readq.nput("cpu.sys %s %s cpu=%s" % (timestamp, cpusystem, cpuid))
+                    self._readq.nput("cpu.irq %s %s cpu=%s" % (timestamp, cpuinterrupt, cpuid))
+                    self._readq.nput("cpu.idle %s %s cpu=%s" % (timestamp, cpuidle, cpuid))
+                    self._readq.nput("cpu.state %s %s" % (int(time.time()), '0'))
+        except Exception as e:
+            self._readq.nput("cpu.state %s %s" % (int(time.time()), '1'))
+            self.log_error("cpus_pctusage collector except exception when parse the filed, abort %s" % e)
 
     def cleanup(self):
         self.log_info('CpusPctusage stop subprocess %d', self.p_top.pid)
