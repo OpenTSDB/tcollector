289d415641dcaa2ffc494f7c009dc2f807cc4644
hehaiyuan <hehaiyuan.hhy@gmail.com>
2017-02-24 00:17:55 +0800

    change tmp path for copy the agent

--- deploy/deploy_multi_machines.sh
+++ deploy/deploy_multi_machines.sh
@@ -10,7 +10,7 @@ ALERTD_SERVER=localhost
 
 function usage() {
   printf "usage: deploy_multi_machines.sh [-h][-update][-deploy]\n"
-  echo "Arguments:"
+  echo " Arguments:"
   echo "    -h            help manual"
   echo "    -update       update collector which have already deploy"
   echo "    -deploy       deploy a new collector"
@@ -25,19 +25,21 @@ if [ "$#" -gt 0 ]; then
         for host in "${HOST[@]}"
         do
             echo "prepare tcollector deploy"
-            scp -r  ${TCOLLECTOR_DIR} ${USERNAME}@${host}:${TCOLLECTOR_DIR}
+            scp -r  ${TCOLLECTOR_DIR} ${USERNAME}@${host}:/tmp/publish
             echo "deploying tcollector for ${USERNAME}@${host}"
             ssh -t  ${USERNAME}@${host} "sudo ORG_TOKEN=${TOKEN} CLIENT_ID=${CLIENT_ID} METRIC_SERVER_HOST=${METRIC_SERVER_HOST} ALERTD_SERVER=${ALERTD_SERVER} ${TCOLLECTOR_DIR}/deploy_agent.sh -update > /tmp/tcollector_udpate.log"
             ssh -t  ${USERNAME}@${host} "sudo /etc/init.d/cloudwiz-agent start > /tmp/tcollector_udpate.log"
+            echo "please login and run /etc/init.d/cloudwiz-agent start"
         done
         exit 0
     elif [ "$1" == "-deploy" ]; then
         for host in "${HOST[@]}"
         do
             echo "prepare tcollector deploy"
-            scp -r  ${TCOLLECTOR_DIR} ${USERNAME}@${host}:${TCOLLECTOR_DIR}
+            scp -r  ${TCOLLECTOR_DIR} ${USERNAME}@${host}:/tmp/publish
             echo "deploying tcollector for ${USERNAME}@${host}"
             ssh -t  ${USERNAME}@${host} "sudo ORG_TOKEN=${TOKEN} CLIENT_ID=${CLIENT_ID} METRIC_SERVER_HOST=${METRIC_SERVER_HOST} ALERTD_SERVER=${ALERTD_SERVER} ${TCOLLECTOR_DIR}/deploy_agent.sh > /tmp/tcollector_udpate.log"
+            echo "please login and run /etc/init.d/cloudwiz-agent start"
         done
         exit 0
     fi
