ba94b080c5f1b18c8b8e84493e6c73c25954a27a
Yongtao You <yongtao_you@yahoo.com>
2017-03-13 11:27:20 +0800

    uagent: Make sure supervisord can stop uagent cleanly

--- uagent/run-uagent.sh
+++ uagent/run-uagent.sh
@@ -6,4 +6,11 @@ if [[ ! -d $log_folder ]]; then
     mkdir "$log_folder"
 fi
 
+function kill_child_process {
+    CHILD_PID=$(pgrep -P $$)
+    kill $CHILD_PID
+}
+
+trap kill_child_process EXIT
+
 ./daemon.py --logfile "$log_folder/uagent.log" "$@"
