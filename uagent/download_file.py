f72e0a908212566b49dbe33dd6ade72cc2ce67bb
Yongtao You <yongtao_you@yahoo.com>
2017-05-10 10:38:15 +0800

    uagent: fix util script download_file.py

--- uagent/download_file.py
+++ uagent/download_file.py
@@ -6,4 +6,5 @@ import uagent
 if (len(sys.argv) != 3):
     print("Usage: {0} <url> <size-limit-bytes>".format(sys.argv[0]))
 else:
-    print(uagent.download_file(sys.argv[1], int(sys.argv[2])))
+    agent = uagent.UAgent(None)
+    print(agent.download_file(sys.argv[1], int(sys.argv[2])))
